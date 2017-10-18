505d5dfd62fd87036ddc582d59638102f2bd2be5
YobertJomi <mengxiangqipa@163.com>
2017-10-18 12:46:13 +0800

    加密

---
+++ Libraries/Library-FrameworkFramework/src/main/java/com/framework/configs/FrameConstant.java
@@ -0,0 +1,10 @@
+package com.framework.configs;
+
+public class FrameConstant
+{
+    public static String IMEI="imei";
+    public static String TOKEN_ID="token_id";
+    public static String SESSION_ID="session_id";
+    public static String SECURITY_KEY="security_key";
+    public static String AES_KEY="aesKey";
+}
