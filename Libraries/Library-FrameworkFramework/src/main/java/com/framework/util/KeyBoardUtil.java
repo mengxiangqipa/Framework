abf67e13731704398e24d536af3ccb4c2e4ebc1c
YobertJomi <mengxiangqipa@163.com>
2019-06-25 21:13:33 +0800

    2019/06/23  UniversalRecyclerview 取消AdapterDataObserver

--- Libraries/Library-FrameworkFramework/src/main/java/com/framework/util/KeyBoardUtil.java
+++ Libraries/Library-FrameworkFramework/src/main/java/com/framework/util/KeyBoardUtil.java
@@ -7,6 +7,7 @@ import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -100,5 +101,78 @@ public class KeyBoardUtil {
     public boolean isKeyboardShown(final @NonNull Context context, final @IntRange(from = 0, to = 1920 * 2) int
             softKeyboardHeight) {
         return isKeyboardShown((((Activity) context).getWindow().getDecorView()), softKeyboardHeight);
+    }
+
+    private View rootView;//activity的根视图
+    private int rootViewVisibleHeight;//纪录根视图的显示高度
+    private OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener;
+
+    private KeyBoardUtil(Activity activity, final @IntRange(from = 0, to = 1920 * 2) int
+            halfSoftKeyboardHeight) {
+        //获取activity的根视图
+        rootView = activity.getWindow().getDecorView();
+
+        //监听视图树中全局布局发生改变或者视图树中的某个视图的可视状态发生改变
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //获取当前根视图在屏幕上显示的大小
+                Rect r = new Rect();
+                rootView.getWindowVisibleDisplayFrame(r);
+
+                int visibleHeight = r.height();
+//                System.out.println("" + visibleHeight);
+                if (rootViewVisibleHeight == 0) {
+                    rootViewVisibleHeight = visibleHeight;
+                    return;
+                }
+
+                //根视图显示高度没有变化，可以看作软键盘显示／隐藏状态没有改变
+                if (rootViewVisibleHeight == visibleHeight) {
+                    return;
+                }
+
+                //根视图显示高度变小超过200，可以看作软键盘显示了
+                if (rootViewVisibleHeight - visibleHeight > halfSoftKeyboardHeight) {
+                    if (onSoftKeyBoardChangeListener != null) {
+                        onSoftKeyBoardChangeListener.keyBoardShow(rootViewVisibleHeight - visibleHeight);
+                    }
+                    rootViewVisibleHeight = visibleHeight;
+                    return;
+                }
+
+                //根视图显示高度变大超过200，可以看作软键盘隐藏了
+                if (visibleHeight - rootViewVisibleHeight > 200) {
+                    if (onSoftKeyBoardChangeListener != null) {
+                        onSoftKeyBoardChangeListener.keyBoardHide(visibleHeight - rootViewVisibleHeight);
+                    }
+                    rootViewVisibleHeight = visibleHeight;
+                    return;
+                }
+            }
+        });
+    }
+
+    private void setOnSoftKeyBoardChangeListener(OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener) {
+        this.onSoftKeyBoardChangeListener = onSoftKeyBoardChangeListener;
+    }
+
+    public interface OnSoftKeyBoardChangeListener {
+        void keyBoardShow(int height);
+
+        void keyBoardHide(int height);
+    }
+
+    /**
+     * 监听键盘弹出
+     *
+     * @param activity                     Activity
+     * @param onSoftKeyBoardChangeListener OnSoftKeyBoardChangeListener
+     */
+    public void setOnSoftKeyBoardChangeListener(Activity activity,
+                                                OnSoftKeyBoardChangeListener onSoftKeyBoardChangeListener,
+                                                final @IntRange(from = 0, to = 1920 * 2) int halfSoftKeyboardHeight) {
+        KeyBoardUtil softKeyBoardListener = new KeyBoardUtil(activity, halfSoftKeyboardHeight);
+        softKeyBoardListener.setOnSoftKeyBoardChangeListener(onSoftKeyBoardChangeListener);
     }
 }
